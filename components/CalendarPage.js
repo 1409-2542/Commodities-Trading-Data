e6fc9b0da6249ff72f639ddbf570d7c003d5a90d
1409-2542 <agna_gabriel@hotmail.com>
2025-05-08 23:07:56 +0100

    adjusted tables to resize for smaller screens

--- components/CalendarPage.js
+++ components/CalendarPage.js
@@ -3,27 +3,29 @@
 export default function Calendar({ data }) {
   return (
     <section className="space-y-6 mt-12">
+      <div className="overflow-x-auto mt-6">
       <h2 className="text-3xl font-bold text-gray-800">Commodity Events Calendar</h2>
         <table className="table-auto w-full bg-white rounded-lg shadow-md mt-6">
           <thead className="bg-gradient-to-r from-blue-700 to-teal-500 text-white">
             <tr>
-              <th className="px-6 py-4 text-left">Commodity</th>
-              <th className="px-6 py-4 text-left">Event</th>
-              <th className="px-6 py-4 text-left">Date</th>
-              <th className="px-6 py-4 text-left">Hour</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Commodity</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Event</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Date</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Hour</th>
             </tr>
           </thead>
           <tbody>
             {data.map((event, index) => (
               <tr key={index} className="hover:bg-gray-100">
-                <td className="px-6 py-4">{event.commodity}</td>
-                <td className="px-6 py-4">{event.event}</td>
-                <td className="px-6 py-4">{event.date}</td>
-                <td className="px-6 py-4">{event.hour}</td>
+                <td className="px-4 py-2 text-left text-sm md:text-base">{event.commodity}</td>
+                <td className="px-4 py-2 text-left text-sm md:text-base">{event.event}</td>
+                <td className="px-4 py-2 text-left text-sm md:text-base">{event.date}</td>
+                <td className="px-4 py-2 text-left text-sm md:text-base">{event.hour}</td>
               </tr>
             ))}
           </tbody>
         </table>
+        </div>
     </section>
   );
 }
--- components/YearlyCommodityTable.js
+++ components/YearlyCommodityTable.js
@@ -4,36 +4,38 @@ export default function YearlyCommodityTable({ data }) {
   return (
     <section className="space-y-6 mt-12">
       <h2 className="text-3xl font-bold text-gray-800">Commodity Prices Table</h2>
+      <div className="overflow-x-auto mt-6">
         <table className="table-auto w-full bg-white rounded-lg shadow-xl mt-6">
           <thead className="bg-gradient-to-r from-blue-700 to-teal-500 text-white">
             <tr>
-              <th className="px-6 py-4 text-left">Commodity</th>
-              <th className="px-6 py-4 text-left">Current Price</th>
-              <th className="px-6 py-4 text-left">Unit</th>
-              <th className="px-6 py-4 text-left">Change</th>
-              <th className="px-6 py-4 text-left">Frequency</th>
-              <th className="px-6 py-4 text-left">Last</th>
-              <th className="px-6 py-4 text-left">Source</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Commodity</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Current Price</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Unit</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Change</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Frequency</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Last</th>
+              <th className="px-4 py-2 text-left text-sm md:text-base">Source</th>
             </tr>
           </thead>
           <tbody>
             {data.map((item, index) => {
               return (
                 <tr key={index} className="hover:bg-gray-100">
-                  <td className="px-6 py-4">{item.name}</td>
-                  <td className="px-6 py-4">{item.price}</td>
-                  <td className="px-6 py-4">{item.unit}</td>
-                  <td className={`px-6 py-4 ${item.change > 0 ? 'text-green-600' : 'text-red-600'}`}>
+                  <td className="px-4 py-2 text-left text-sm md:text-base">{item.name}</td>
+                  <td className="px-4 py-2 text-left text-sm md:text-base">{item.price}</td>
+                  <td className="px-4 py-2 text-left text-sm md:text-base">{item.unit}</td>
+                  <td className={`px-4 py-2 text-left text-sm md:text-base ${item.change > 0 ? 'text-green-600' : 'text-red-600'}`}>
                     {item.change}%
                   </td>
-                  <td className="px-6 py-4">{item.frequency}</td>
-                  <td className="px-6 py-4">{item.last}</td>
-                  <td className="px-6 py-4">{item.source}</td>
+                  <td className="px-4 py-2 text-left text-sm md:text-base">{item.frequency}</td>
+                  <td className="px-4 py-2 text-left text-sm md:text-base">{item.last}</td>
+                  <td className="px-4 py-2 text-left text-sm md:text-base">{item.source}</td>
                 </tr>
               )
             })}
           </tbody>
         </table>
+        </div>
     </section>
   )
 }
